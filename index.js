9fa4f355edef557a3757b143003c8d89bcfb0212
hasan <97052700+HasanAlzubeidi@users.noreply.github.com>
2022-04-16 00:01:59 +0200

    line 1

---
+++ index.js
@@ -0,0 +1,17 @@
+// document.getElementById("count-el").innerText=5
+
+let countEL=document.getElementById("count-el")
+let count = 0
+//you have to invoke the function
+function increment(){
+    count=count+1
+    countEL.innerText=count
+    console.log("hasan")
+    
+}
+
+
+
+
+
+
