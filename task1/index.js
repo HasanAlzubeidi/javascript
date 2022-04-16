95b26a0ae14692ff1eb5c8dddfc2dd98981afacd
hasan <97052700+HasanAlzubeidi@users.noreply.github.com>
2022-04-16 02:08:50 +0200

    the first app is almost finsished

--- task1/index.js
+++ task1/index.js
@@ -1,11 +1,23 @@
 // document.getElementById("count-el").innerText=5
+//use textContent instead of innerText 
 
+let saveEL=document.getElementById("save-el")
 let countEL=document.getElementById("count-el")
 let count = 0
 //you have to invoke the function
 function increment(){
-    count=count+1
+    count+=1
     countEL.innerText=count
-    console.log("hasan")
-
 }
+
+
+function save(){
+let temp = count + " - "
+saveEL.textContent +=temp
+}
+
+function reset(){
+    count = 0
+    countEL.innerText=count
+}
+
