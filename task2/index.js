c2c6b912a211f6ac55dda37d486be974ade1c3d1
hasan <97052700+HasanAlzubeidi@users.noreply.github.com>
2022-04-16 02:10:49 +0200

    s

---
+++ task2/index.js
@@ -0,0 +1,10 @@
+
+
+let welcomEL=document.getElementById("welcom-el")
+
+let nameUser = "Hasan"
+let greeting = " welcom to the programming world"
+
+welcomEL.innerText = nameUser + greeting
+
+welcomEL.innerText += " SVT"
